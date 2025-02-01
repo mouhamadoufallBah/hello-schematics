4ca244e8e02f0f8f30f9b8c70204c2fa807b5487
mouhamadoufallBah <mouhamadoufallbah@gmail.com>
2025-02-01 01:25:37 +0000

    Add generated file from schematics

--- src/hello-schematics/index.ts
+++ src/hello-schematics/index.ts
@@ -5,6 +5,7 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 // per file.
 export function helloSchematics(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    tree.create('hello.txt', 'Ceci est un fichier généré par mon schématics');
     return tree;
   };
 }
