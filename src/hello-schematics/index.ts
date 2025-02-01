84a9d4682edd213286704c4fe54917c9e30c9449
mouhamadoufallBah <mouhamadoufallbah@gmail.com>
2025-02-01 01:54:37 +0000

    Add dynamic hello file generation with template

--- src/hello-schematics/index.ts
+++ src/hello-schematics/index.ts
@@ -1,12 +1,17 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { apply, mergeWith, Rule, SchematicContext, strings, template, Tree, url } from '@angular-devkit/schematics';
+
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function helloSchematics(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    const message = ` Ceci est un fichier généré par  ${_options.nom}!`;
-    tree.create('hello_dynamique.txt', message);
-    return tree;
+   const templateSource = apply(url('./files'), [
+    template({
+      ...strings,
+      ..._options
+    })
+   ])
+    return mergeWith(templateSource)(tree, _context);
   };
 }
